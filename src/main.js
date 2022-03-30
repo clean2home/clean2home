f08ee6e474ce72af65c6cb413434f277e46fa631
Alejandro García <alejandrogarciaolmeda@gmail.com>
2022-03-30 13:44:27 +0200

    Carrusel añadido

--- src/main.js
+++ src/main.js
@@ -1,2 +1,38 @@
-console.log("Prueba Alex");
-console.log("Prueba");
+window.addEventListener("load", () => {
+  // slide incial
+  let slide = 1;
+
+  // total slides
+  const slides = document.querySelectorAll(".slider ul li");
+  const total = slides.length;
+
+  showSlide(1);
+
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+
+  next.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    slide++;
+    if (slide > total) {
+      slide = 1;
+    }
+    showSlide(slide);
+  });
+
+  prev.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    slide--;
+    if (slide < 1) {
+      slide = total;
+    }
+    showSlide(slide);
+  });
+
+  function showSlide(n) {
+    n--; // decrement 1
+    for (let i = 0; i < slides.length; i++) {
+      (i === n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
+    }
+  }
+});
