4a691200b2fed6f8e8f43e9bb8a5f50c9782e386
riskezwn <enriquegarciagasc@gmail.com>
2022-04-08 15:02:18 +0200

    Añadido carrusel
    Añadido librería GlideJS para el carrusel de la web, y estilo de
    tarjetas. Corrección menor de linter para permitir clases, y
    correcciones menores de spacing (margenes y paddings), y cambio de orden
    elementos CSS

--- src/main.js
+++ src/main.js
@@ -1,47 +1,25 @@
 import Glide from "@glidejs/glide";
 import "@glidejs/glide/dist/css/glide.core.min.css";
+import "@glidejs/glide/dist/css/glide.theme.min.css";
 
+// Carousel
 new Glide(".glide", {
   type: "carousel",
   focusAt: "center",
-  perView: 3,
-}).mount();
-
-/* window.addEventListener("load", () => {
-  // slide incial
-  let slide = 1;
-
-  // total slides
-  const slides = document.querySelectorAll(".slider ul li");
-  const total = slides.length;
-
-  showSlide(1);
-
-  const next = document.querySelector(".next");
-  const prev = document.querySelector(".prev");
-
-  next.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    slide++;
-    if (slide > total) {
-      slide = 1;
-    }
-    showSlide(slide);
-  });
-
-  prev.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    slide--;
-    if (slide < 1) {
-      slide = total;
-    }
-    showSlide(slide);
-  });
-
-  function showSlide(n) {
-    n--; // decrement 1
-    for (let i = 0; i < slides.length; i++) {
-      (i === n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
+  perView: 4,
+  gap: 70,
+  autoplay: 7000,
+  keyboard: true,
+  breakpoints: {
+    1400: {
+      perView: 3
+    },
+    1024: {
+      perView: 2,
+      gap: 70
+    },
+    600: {
+      perView: 1
     }
   }
-}); */
+}).mount();
