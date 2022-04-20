81290adedca6104d721cd2b17c4742dd57bd3c69
Alejandro García <alejandrogarciaolmeda@gmail.com>
2022-04-20 11:55:24 +0200

    Iniciada configuración Firebase

---
+++ src/components/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore();
+
+export { db, auth };
