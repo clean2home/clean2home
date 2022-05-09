71bfb204725236cdaa38540e9c5955767657adf2
Enrique García <59698656+riskezwn@users.noreply.github.com>
2022-05-09 17:24:32 +0200

    Cambio estructura para obtener datos de Firebase (encapsulada asincronia del documento en funciones) (#29)

--- src/components/cleaners.js
+++ src/components/cleaners.js
@@ -1,46 +1,58 @@
-import { getFirestore, getDocs, collection } from "firebase/firestore/lite";
-import { app } from "./firebase/config";
-
-const db = getFirestore(app);
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "./firebase/config";
 
 const divContainerCards = document.querySelector(".container-cards");
 
-const cleaners = [];
-const snap = await getDocs(collection(db, "cleaners"));
-snap.forEach((doc) => {
-  cleaners.push(doc.data());
-});
+const getCleaners = async() => {
+  const cleaners = [];
+  const collRef = collection(db, "cleaners");
+  const querySnapshot = await getDocs(collRef);
+  querySnapshot.forEach((doc) => {
+    cleaners.push({
+      id: doc.id,
+      ...doc.data()
+    });
+  });
 
-function shortComment(element) { // Se le pasa un elemento directamente
-  if (element.textContent.length > 190) element.textContent = `${element.textContent.substring(0, 190)}...`;
-}
+  return cleaners;
+};
 
-cleaners.forEach(cleaner => {
-  const cleanerCard = `<div class="cleaner-container">
-  <div class="cleaner-image"> <!-- imagen -->
-    <img src="${cleaner.image}"  class="services-profile">
-  </div>
-  <div class="cleaner-info"><!-- info -->
-    <div class="container-name-rating">
-    <div class="container-name-city>
-      <h3 class="name-cleaner"><strong>${cleaner.name}</strong> ${cleaner.verified}
-      <h4 class="city-cleaner"><small>📍${cleaner.city}</small></h4></div>
-      </h3>
-    <div class="rating-star">
-      <p class="rating"><strong><i class="fa-solid fa-star"></i>  ${cleaner.rating}</strong></p>
+const printCleaners = async() => {
+  const cleaners = await getCleaners();
+
+  const shortComment = (element) => { // Se le pasa un elemento directamente
+    if (element.textContent.length > 190) element.textContent = `${element.textContent.substring(0, 190)}...`;
+  };
+
+  cleaners.forEach(cleaner => {
+    const cleanerCard = `<div class="cleaner-container">
+    <div class="cleaner-image"> <!-- imagen -->
+      <img src="${cleaner.image}"  class="services-profile">
+    </div>
+    <div class="cleaner-info"><!-- info -->
+      <div class="container-name-rating">
+      <div class="container-name-city>
+        <h3 class="name-cleaner"><strong>${cleaner.name}</strong> ${cleaner.verified}
+        <h4 class="city-cleaner"><small>📍${cleaner.city}</small></h4></div>
+        </h3>
+      <div class="rating-star">
+        <p class="rating"><strong><i class="fa-solid fa-star"></i>  ${cleaner.rating}</strong></p>
+      </div>
+      </div >
+      <p class="cleaner-info-p">${cleaner.description}</p>
+      <p class="works">${cleaner.works} trabajos</p>
+    </div >
+    <div class="cleaner-btn"><!-- precio/boton -->
+      <p class="price"><strong> ${cleaner.price}</strong><small>€/hora</small></p>
+      <a href="/under-construction.html" class="btn hire">Contratar</a>
     </div>
     </div >
-    <p class="cleaner-info-p">${cleaner.description}</p>
-    <p class="works">${cleaner.works} trabajos</p>
-  </div >
-  <div class="cleaner-btn"><!-- precio/boton -->
-    <p class="price"><strong> ${cleaner.price}</strong><small>€/hora</small></p>
-    <a href="/under-construction.html" class="btn hire">Contratar</a>
-  </div>
-  </div >
-  `;
-  divContainerCards.innerHTML += cleanerCard;
-});
+    `;
+    divContainerCards.innerHTML += cleanerCard;
+  });
 
-const descriptionParagraph = document.querySelectorAll(".cleaner-info-p");
-descriptionParagraph.forEach(e => shortComment(e));
+  const descriptionParagraph = document.querySelectorAll(".cleaner-info-p");
+  descriptionParagraph.forEach(e => shortComment(e));
+};
+
+printCleaners();
--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -15,10 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const auth = getAuth();
-const db = getFirestore();
-
-export { db, auth };
-export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
